86a7f1d03ab2f34a4fbf46d3e019cec513ee41d0
Renan Souza <renan@klever.io>
2024-04-16 21:15:55 +0100

    docker-compose with prometheus and grafana configurations

---
+++ src/test/java/pt/erudio/apigateway/StartupTests.java
@@ -0,0 +1,13 @@
+package pt.erudio.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
